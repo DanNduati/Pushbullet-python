a72a5bc5d3a03199c0e6eb12510691f977aedc6d
Daniel Nduati <dannduati2@gmail.com>
2021-11-11 18:12:26 +0300

    Update v1.py
    Use the pushbullet.py python client which is a python wrapper for the
    pushbullet api. Test authentication and list devices an authenticated
    user has access to

--- v1.py
+++ v1.py
@@ -1,9 +1,15 @@
 import os
 from os.path import join,dirname
 from dotenv import load_dotenv
+from pushbullet import Pushbullet
 
 dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
 api_key = os.environ.get('API_KEY','default')
-print(api_key)
+
+#Authentication
+pb = Pushbullet(api_key)
+
+#get all devices the current user has access to
+print(pb.devices)
